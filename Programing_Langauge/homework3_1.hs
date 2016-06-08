178842c84fce6c40b47bfdc93e5853af11eac0be
chikinmukjja <pjw921116@hanyang.ac.kr>
2016-06-08 20:04:54 +0900

    Delete homework3_1.hs

--- Programing_Langauge/homework3_1.hs
+++
@@ -1,9 +0,0 @@
-
-primes = getPrime[2..]
-	where
-	getPrime (p:xs) = p : getPrime[x | x <- xs, x `mod` p > 0]
-
-findingPrimes n size  
-		| n < 1 = take size (filter ( > 1) primes )
-		| size < 1 = []  
-		|  otherwise = take size (filter ( > n) primes )
